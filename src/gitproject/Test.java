e88335af7e170841a956a44a5f0513f6807238c4
student <student@FJ-1025>
2026-05-15 12:17:22 +0900

    Test2クラスのフィールドを使用

--- src/gitproject/Test.java
+++ src/gitproject/Test.java
@@ -3,5 +3,7 @@ package gitproject;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		Test2 test2 = new Test2();
+		System.out.println(test2.x + test2.y);
 	}
 }
