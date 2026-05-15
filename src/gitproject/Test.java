8d79fd726a92a89925779eb16c4875d9b420ddfa
student <student@FJ-1025>
2026-05-15 13:32:41 +0900

    ファイル名を変更

--- src/gitproject/Test.java
+++ src/gitproject/Test.java
@@ -1,9 +1,6 @@
 package gitproject;
 
 public class Test {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-		Test2 test2 = new Test2();
-		System.out.println(test2.x + test2.y);
-	}
+	public int x = 1;
+	public int y = 2;
 }
--- src/gitproject/Test2.java
+++
@@ -1,6 +0,0 @@
-package gitproject;
-
-public class Test2 {
-	public int x = 1;
-	public int y = 2;
-}
